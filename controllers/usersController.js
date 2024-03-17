f1e5ca9680d7fbf312b8f4bbf17c472ad12b0626
Andrew Sergienko <underway336@gmail.com>
2024-03-18 01:15:59 +0300

    added controllers/usersController.js

---
+++ controllers/usersController.js
@@ -0,0 +1,26 @@
+import connection from '../database/db.js';
+import status from '../response.js';
+
+export function users(req, res) {
+  connection.query('SELECT * FROM `users`', (error, rows, fields) => {
+    if (error) {
+      console.log(error);
+    } else {
+      status(rows, res);
+    }
+  });
+}
+
+export function add(req, res) {
+  const sql = 'INSERT INTO `users`(`name`, `second_name`, `email`) VALUES(?, ?, ?)';
+  const values = [req.query.name, req.query.second_name, req.query.email];
+
+  connection.query(sql, values, (error, results) => {
+    if (error) {
+      console.log(error);
+    } else {
+      status(results, res);
+    }
+  });
+  console.log(req.query);
+}
